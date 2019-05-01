643932665f43b3874111ff4c61666faa19f4784d
AdrianDolniak <adriandolniak.wp.pl>
2019-05-01 12:44:17 +0200

    clean up

--- showing off behave/features/environment.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-def before_feature(context, feature):
-    context.driver = webdriver.Chrome()
-    context.driver.implicitly_wait(2)
-
-
-def after_feature(context, feature):
-    context.driver.quit()
