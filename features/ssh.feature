47cc89ce6acb54e086414b4e2c5357cbd2e7b0d5
AdrianDolniak <adriandolniak.wp.pl>
2019-05-21 19:33:24 +0200

    ssh test update

--- features/ssh.feature
+++ features/ssh.feature
@@ -1,18 +1,18 @@
 Feature: Testing remote hosts with Ssh library
 
-  Scenario: Check if we are connected with remote host
-     Given the connection is set with remote host
-      When we check if there is a connection with remote host
-      Then the response contain phrase "adi-vmware"
+  Scenario: Connection with remote host
+     Given we open the terminal
+      When we check the connection with remote host
+      Then the response should contain a phrase "adi-vmware"
 
 
-  Scenario: Check if the host machine has processor with 4 cores
-     Given the connection is set with remote host
-      When we check if remote machine has 4 cores
-      Then the response is "4"
+  Scenario: Remote machine with 4 cores processor
+     Given we open the terminal
+      When we check if the remote machine processor has 4 cores
+      Then the response should be "4"
 
 
-  Scenario: Check if we are connected to Internet on host
-     Given the connection is set with remote host
-      When we check if remote machine has internet connection
-      Then the response contain phrase "1 received"
+  Scenario: Connection to Internet on remote host
+     Given we open the terminal
+      When we check if the remote machine has internet connection
+      Then the response should contain phrase "1 received"
--- features/steps/ssh.py
+++ features/steps/ssh.py
@@ -3,56 +3,50 @@ import paramiko
 from hamcrest import *
 
 # givens
-@given(u'the connection is set with remote host')
+@given(u'we open the terminal')
 def step_impl(context):
     pass
-
 
 # whens
-@when(u'we check if there is a connection with remote host')
-def step_impl(context):
-    pass
-
-
-@when(u'we check if remote machine has 4 cores')
-def step_impl(context):
-    pass
-
-
-@when(u'we check if remote machine has internet connection')
-def step_impl(context):
-    pass
-
-# thens
-@then(u'the response contain phrase "adi-vmware"')
+@when(u'we check the connection with remote host')
 def step_impl(context):
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect("localhost", port=22, username="adi", password="adi!", allow_agent=False, look_for_keys=False)
     stdin, stdout, stderr = ssh.exec_command("uname -a")
-    output = stdout.read()
-    print (output)
-    assert_that(output, contains_string("adi-vmware"))
+    context.output = stdout.read()
 
-@then(u'the response is "4"')
+@when(u'we check if the remote machine processor has 4 cores')
 def step_impl(context):
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect("localhost", port=22, username="adi", password="adi!", allow_agent=False, look_for_keys=False)
     stdin, stdout, stderr = ssh.exec_command("nproc")
-    output = stdout.read()
-    print (output)
-    assert_that(output, contains_string("4"))
+    context.output = stdout.read()
 
-@then(u'the response contain phrase "1 received"')
+
+@when(u'we check if the remote machine has internet connection')
 def step_impl(context):
     ssh = paramiko.SSHClient()
     ssh.load_system_host_keys()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect("localhost", port=22, username="adi", password="adi!", allow_agent=False, look_for_keys=False)
     stdin, stdout, stderr = ssh.exec_command("ping -c1 8.8.8.8")
-    output = stdout.read()
-    print (output)
-    assert_that(output, contains_string("1 received"))
+    context.output = stdout.read()
+
+# thens
+@then(u'the response should contain a phrase "adi-vmware"')
+def step_impl(context):
+    assert_that(context.output, contains_string("adi-vmware"))
+
+
+@then(u'the response should be "4"')
+def step_impl(context):
+    assert_that(context.output, contains_string("4"))
+
+
+@then(u'the response should contain phrase "1 received"')
+def step_impl(context):
+    assert_that(context.output, contains_string("1 received"))
